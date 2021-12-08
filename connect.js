4c74665b55f91e04f780ce004d845562306d6a68
Andriy Parkhomenko <55849068+AndriyParkho@users.noreply.github.com>
2021-12-08 10:51:53 +0100

    Update connect.js

--- connect.js
+++ connect.js
@@ -10,6 +10,7 @@ var recursiveAsyncReadLine = function () {
     rl.question('Mot de passe: ', function (answer) {
         exec(sq.quote(['./.serveur/checkPassword.sh', answer]), function(err, stdout, stderr) {
             reponse = stdout.replace(/(\r\n|\n|\r)/gm, "")
+            console.log("Mot de passe entrée : ", stdout)
             if(reponse == 'true'){
                 console.log("Vous êtes connecté");
                 return rl.close();
